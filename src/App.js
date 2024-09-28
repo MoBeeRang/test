43ca5512feb903fd6ef52bb59ec1f01d10bd4fc7
MoBeeRang <preepree@naver.com>
2024-09-28 21:33:03 +0900

    test commit2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 1123213123123
+          Learn React
         </a>
       </header>
     </div>
