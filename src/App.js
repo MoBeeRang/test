a6876901549e553ac3019ae55b76f18ffb4466ec
MoBeeRang <preepree@naver.com>
2024-09-28 21:00:15 +0900

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 1123213
         </a>
       </header>
     </div>
